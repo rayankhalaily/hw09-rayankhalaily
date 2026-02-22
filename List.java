3f3f65e114de42683549c8797cea6497b7c0aab4
rayankhalaily <rayan.khalaily@post.runi.ac.il>
2026-02-22 22:23:24 +0200

    Refactor List class methods and add remove functionality

--- List.java
+++ List.java
@@ -1,99 +1,86 @@
-/** A linked list of character data objects.
- *  (Actually, a list of Node objects, each holding a reference to a character data object.
- *  However, users of this class are not aware of the Node objects. As far as they are concerned,
- *  the class represents a list of CharData objects. Likwise, the API of the class does not
- *  mention the existence of the Node objects). */
 public class List {
-
-    // Points to the first node in this list
     private Node first;
-
-    // The number of elements in this list
     private int size;
-	
-    /** Constructs an empty list. */
+
     public List() {
         first = null;
         size = 0;
     }
-    
-    /** Returns the number of elements in this list. */
+
     public int getSize() {
- 	      return size;
+        return size;
     }
 
-    /** Returns the CharData of the first element in this list. */
+    // זה החלק שהיה חסר והפיל את הטסטים
     public CharData getFirst() {
-        // Your code goes here
+        if (first == null) return null;
         return first.cp;
     }
 
-    /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
-        // Your code goes here
-    }
-    
-    /** GIVE Textual representation of this list. */
-    public String toString() {
-        // Your code goes here
-        return "";
+        Node newNode = new Node(new CharData(chr));
+        newNode.next = first;
+        first = newNode;
+        size++;
     }
 
-    /** Returns the index of the first CharData object in this list
-     *  that has the same chr value as the given char,
-     *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
-        // Your code goes here
-        return -1;
-    }
-
-    /** If the given character exists in one of the CharData objects in this list,
-     *  increments its counter. Otherwise, adds a new CharData object with the
-     *  given chr to the beginning of this list. */
-    public void update(char chr) {
-        // Your code goes here
-    }
-
-    /** GIVE If the given character exists in one of the CharData objects
-     *  in this list, removes this CharData object from the list and returns
-     *  true. Otherwise, returns false. */
-    public boolean remove(char chr) {
-        // Your code goes here
-        return false;
-    }
-
-    /** Returns the CharData object at the specified index in this list. 
-     *  If the index is negative or is greater than the size of this list, 
-     *  throws an IndexOutOfBoundsException. */
-    public CharData get(int index) {
-        // Your code goes here
-        return null;
-    }
-
-    /** Returns an array of CharData objects, containing all the CharData objects in this list. */
-    public CharData[] toArray() {
-	    CharData[] arr = new CharData[size];
-	    Node current = first;
-	    int i = 0;
+        Node current = first;
+        int i = 0;
         while (current != null) {
-    	    arr[i++]  = current.cp;
-    	    current = current.next;
-        }
-        return arr;
-    }
-
-    /** Returns an iterator over the elements in this list, starting at the given index. */
-    public ListIterator listIterator(int index) {
-	    // If the list is empty, there is nothing to iterate   
-	    if (size == 0) return null;
-	    // Gets the element in position index of this list
-	    Node current = first;
-	    int i = 0;
-        while (i < index) {
+            if (current.cp.chr == chr) {
+                return i;
+            }
             current = current.next;
             i++;
         }
-        // Returns an iterator that starts in that element
-	    return new ListIterator(current);
+        return -1;
+    }
+
+    public void update(char chr) {
+        int index = indexOf(chr);
+        if (index == -1) {
+            addFirst(chr);
+        } else {
+            get(index).count++;
+        }
+    }
+
+    public CharData get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.cp;
+    }
+
+    public boolean remove(char chr) {
+        Node prev = null;
+        Node current = first;
+        while (current != null && current.cp.chr != chr) {
+            prev = current;
+            current = current.next;
+        }
+        if (current == null) return false;
+        if (prev == null) first = current.next;
+        else prev.next = current.next;
+        size--;
+        return true;
+    }
+
+    public String toString() {
+        if (size == 0) return "()";
+        StringBuilder str = new StringBuilder("(");
+        Node current = first;
+        while (current != null) {
+            str.append(current.cp + " ");
+            current = current.next;
+        }
+        str.deleteCharAt(str.length() - 1);
+        str.append(")");
+        return str.toString();
     }
 }
